4f691651aa53ec94f21738a7594da54a1db10168
jasongauvin <contact.jason.gauvin@gmail.com>
2021-03-14 18:57:37 +0100

    feat: Modify structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Todo from "../views/Todo.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Todo",
+    component: Todo
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/list",
+    name: "List",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function() {
-      return import(/* webpackChunkName: "about" */ "../views/About.vue");
+      return import(/* webpackChunkName: "about" */ "../views/List.vue");
     }
   }
 ];
---
+++ src/views/Todo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <todo-header></todo-header>
+    <todo-table></todo-table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoTable from "@/components/TodoTable.vue";
+import TodoHeader from "@/components/TodoHeader.vue";
+
+export default {
+  name: "Todo",
+  components: {
+    TodoTable,
+    TodoHeader
+  }
+};
+</script>
